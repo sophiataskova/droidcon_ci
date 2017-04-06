4393ffaf796508903a2f1c89047bab2ee033e31b
sophiataskova <sofiya@hipmunk.com>
2017-04-06 12:22:17 +0200

    first

---
+++ Droidcon_CI/app/src/main/java/com/example/sophiataskova/droidcon_ci/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sophiataskova.droidcon_ci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
